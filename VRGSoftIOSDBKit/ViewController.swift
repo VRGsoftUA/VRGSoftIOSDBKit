6f046d06b058d057728adacc92acf2db016850ae
Maksym Vereshchaka <m.vereshaka@vrgsoft.net>
2020-03-05 17:56:31 +0200

    Add project and core data files

---
+++ VRGSoftIOSDBKit/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  VRGSoftIOSDBKit
+//
+//  Created by developer on 05.03.2020.
+//  Copyright © 2020 OLEKSANDR SEMENIUK. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
